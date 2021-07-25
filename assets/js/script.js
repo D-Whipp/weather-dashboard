0d2a129cd35c339e235691eeec40cf334565ed5b
Mr Whipp <dwhipp88@gmail.com>
2021-07-25 15:49:06 -0600

    working on api request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,29 @@
-var cityName =
-  "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-var apiKey = "b27d174b72113aa4b9e36f1b858b0248";
+// API CALL FOR CITY = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+// ^^^ IMPORTANT ^^^
+
+var apiKey = "&appid=b27d174b72113aa4b9e36f1b858b0248";
 var apiURL =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,daily&appid=";
+  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,daily";
+
+var apiCityCall = "api.openweathermap.org/data/2.5/weather?q=";
 
 // Query Selectors
 var userFormEl = document.querySelector("#user-form");
 var searchInputEl = document.querySelector("#search-item");
 
-
-var getResponse = function () {
-  fetch(apiURL + apiKey).then(function (response) {
-    response.json().then(function (data) {
-      console.log(data);
-    });
-  });
+var getWeatherData = function (searchTerm) {
+  // format the weather api url
+  var cityApiUrl = apiCityCall + searchTerm + apiKey;
+  
+  // make a request to the url
+  fetch(cityApiUrl).then(function (response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      })
+    }
+  })
 };
 
 var formSubmitHandler = function (event) {
@@ -22,8 +31,12 @@ var formSubmitHandler = function (event) {
 
   // get value from input element
   var searchTerm = searchInputEl.value.trim();
-  console.log(searchTerm);
+
+  if (searchTerm) {
+    getWeatherData(searchTerm);
+    searchInputEl.value = "";
+  } 
 };
 
-getResponse();
+
 userFormEl.addEventListener("click", formSubmitHandler);
