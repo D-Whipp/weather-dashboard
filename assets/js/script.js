f0b7ad5f6ff08397f967f249fa3069b1e487132d
Mr Whipp <dwhipp88@gmail.com>
2021-07-25 15:12:46 -0600

    added favicon | logic can now capture entered text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
-// Open weather one call api
-// API KEY b27d174b72113aa4b9e36f1b858b0248
+var cityName =
+  "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 var apiKey = "b27d174b72113aa4b9e36f1b858b0248";
+var apiURL =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,daily&appid=";
+
+// Query Selectors
+var userFormEl = document.querySelector("#user-form");
+var searchInputEl = document.querySelector("#search-item");
+
 
 var getResponse = function () {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey);
-}
+  fetch(apiURL + apiKey).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  // get value from input element
+  var searchTerm = searchInputEl.value.trim();
+  console.log(searchTerm);
+};
 
 getResponse();
+userFormEl.addEventListener("click", formSubmitHandler);
