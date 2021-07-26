245c4ca3205e7e03007a2322b6823c0fe3e94de8
Mr Whipp <dwhipp88@gmail.com>
2021-07-25 19:00:01 -0600

    update weather report to include city name and current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 var apiKey = "&appid=b27d174b72113aa4b9e36f1b858b0248";
-var apiURL =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&units=imperial&exclude=hourly,daily";
+var searchByCity = "https://api.openweathermap.org/data/2.5/weather?q=";
+// converts units to imperial
+var apiUnits = "&units=imperial";
+var now = moment().format("MMMM Do YYYY");
 
-var apiCityCall = "api.openweathermap.org/data/2.5/weather?q=";
+// weather report variables
+var cityNameEl = document.querySelector(".city-name");
+var todayEl = document.querySelector(".today");
+console.log(now);
 
 // Query Selectors
 var userFormEl = document.querySelector("#user-form");
@@ -10,17 +15,20 @@ var searchInputEl = document.querySelector("#search-item");
 
 var getWeatherData = function (searchTerm) {
   // format the weather api url
-  var cityApiUrl = apiCityCall + searchTerm + apiKey;
-  
+  var cityApiUrl = searchByCity + searchTerm + apiUnits + apiKey;
+
   // make a request to the url
   fetch(cityApiUrl).then(function (response) {
     // request was successful
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
-      })
+        cityNameEl.innerHTML = searchTerm;
+        todayEl.innerHTML = now;
+      });
+    } else {
+      alert("You need to enter in a city found in the USA!")
     }
-  })
+  });
 };
 
 var formSubmitHandler = function (event) {
@@ -32,8 +40,7 @@ var formSubmitHandler = function (event) {
   if (searchTerm) {
     getWeatherData(searchTerm);
     searchInputEl.value = "";
-  } 
+  }
 };
-
 
 userFormEl.addEventListener("click", formSubmitHandler);
